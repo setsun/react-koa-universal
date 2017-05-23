5fdb0e7fd8e9e17cf7cf5744e70419f42273af41
Sean Je Luo <setsun@setsun.io>
2017-05-23 00:08:22 -0400

    more stuff like koa, server-side rendering and eject react-scripts

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
-const Koa = require('koa');
-const app = new Koa();
+const path = require('path');
 
-app.use(ctx => {
-  ctx.body = 'Setsun\'s World';
+const koa = require('koa');
+const reactView = require('koa-react-view');
+const register = require('babel-register');
+
+const app = new koa();
+
+const reactViewsPath = path.join(__dirname, 'containers');
+
+reactView(app, {
+  views: reactViewsPath
 });
 
-app.listen(3000);
+register({
+  presets: ['react-app'],
+  extensions: ['.jsx'],
+});
+
+app.use(function* () {
+  this.render('Index', {
+    title: 'Index',
+    list: [
+      'hello koa',
+      'hello react'
+    ]
+  });
+});
+
+app.listen(8818);
+console.log('Started!');
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Intro = styled.p`
+  font-size: large;
+`;
 
 class App extends Component {
   render() {
@@ -10,9 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Intro>
       </div>
     );
   }
